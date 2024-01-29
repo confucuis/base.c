0e7a12125f365781c17e88f7c5009a775532d247
confucuis <confucuis@github.com>
2024-01-29 14:45:59 +0800

    ..

--- src/callback_fn.c
+++ src/callback_fn.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 
+typedef void (*callback)(int);
+
 // 回调函数的定义
 void callbackFunc(int arg) {
     // 执行回调函数的操作
@@ -7,11 +9,11 @@ void callbackFunc(int arg) {
 }
 
 // 注册回调函数的函数
-void registerCallback(void (*callback)(int)) {
+void registerCallback(callback func) {
     // 某些操作...
     // 当满足特定条件时调用回调函数
     int arg = 10;
-    callback(arg);
+    func(arg);
 }
 
 int main() {
