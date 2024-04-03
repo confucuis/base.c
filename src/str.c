cd30503aba36c3c8f31abe19ff8d129ff28f22af
confucuis <confucuis@github.com>
2024-04-03 17:49:05 +0800

    ..

--- src/str.c
+++ src/str.c
@@ -17,9 +17,8 @@ int main()
     printf("%s\n", s1);  // out: abcabc
     printf("%s\n", s2);  // out: abc
 
-    size_t s1len, s2len;
-    s1len = strlen(s1);
-    s2len = strlen(s2);
+    size_t s1len = strlen(s1);
+    size_t s2len = strlen(s2);
     s1len > s2len ? printf("s1len = %ld\n", s1len) : printf("s2len = %ld\n", s2len);
 
     return 0;
