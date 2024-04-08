ab8d10fbb1cde4b650f5b92406d92391da816fe1
confucuis <confucuis@github.com>
2024-04-08 16:43:58 +0800

    ..

--- src/str.c
+++ src/str.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <string.h>
+#include <ctype.h> // 字符判断头文件
 
 int main()
 {
@@ -20,6 +21,13 @@ int main()
     size_t s1len = strlen(s1);
     size_t s2len = strlen(s2);
     s1len > s2len ? printf("s1len = %ld\n", s1len) : printf("s2len = %ld\n", s2len);
+
+    // 字符操作
+    int r = isalpha('?');
+    if (r == 0)
+        printf("非字母\n");
+    else
+        printf("是字母\n");
 
     return 0;
 }
