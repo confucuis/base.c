231fbfea8914e687d51081458ff50913ac3f51f9
confucuis <confucuis@github.com>
2024-04-13 17:30:46 +0800

    ..

--- src/cc/pointer.cc
+++ src/cc/pointer.cc
@@ -1,3 +1,4 @@
+#include <memory>
 #include <string>
 #include <iostream>
 
@@ -11,6 +12,19 @@ int main()
     name = &second_name;
     *name = "李四";
     std::cout << first_name + "\n" << second_name << std::endl;
+
+    // 堆上分配内存
+    std::string *ss = new std::string("hello world!");
+    std::cout << *ss << std::endl;
+    delete ss;   
+
+    // 智能指针
+    /*独占所有权的智能指针*/
+    std::unique_ptr<std::string> foo(new std::string("foo!"));
+    std::cout << *foo << std::endl;
+    /*多个指针共享对象的智能指针*/
+    std::shared_ptr<std::string> bar(new std::string("bar!"));
+    std::cout << *bar << std::endl;
 
     return 0;
 }
