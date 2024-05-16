0ab74761b6e948fc5b6765086e87e2bee0f15930
confucuis <confucuis@github.com>
2024-05-16 09:17:54 +0800

    ..

--- src/cc/template.cc
+++ src/cc/template.cc
@@ -15,8 +15,15 @@ public:
   }
 };
 
+template <typename T>
+T add(T a, T b)
+{
+    return a + b;
+}
+
 int main()
 {
+  // template class
   Arithmetic<int> iarithmetic;
   std::cout << iarithmetic.add(3, 5) << std::endl;
   std::cout << iarithmetic.subtract(4, 1) << std::endl;
@@ -24,6 +31,10 @@ int main()
   Arithmetic<float> farithmetic;
   std::cout << farithmetic.add(3.3, 4.5) << std::endl;
   std::cout << farithmetic.subtract(7.7, 2.3) << std::endl;
+
+  // template func
+  std::cout << add(3, 5) << std::endl;
+  std::cout << add(4.5, 6.7) << std::endl;
 
   return 0;
 }
