ced965adf60ff22618c55a2e463e39ea6df2b72f
confucuis <confucuis@github.com>
2024-06-22 17:44:29 +0800

    ..

--- src/cc/virtual.cc
+++ src/cc/virtual.cc
@@ -1,15 +1,15 @@
 #include <string>
 #include <iostream>
 
+// 1. 虚函数,可以被派生类重写.
+// 2. 并在运行时动态绑定到对象的实际类型.
+// 3. 通过基类指针或引用调用该虚函数时,
+//    程序会根据实际对象的类型调用正确的函数版本,
+//    而不是仅仅根据指针或引用的类型来决定调用哪个函数.
 
 class Base
 {
 public:
-    // 1. 虚函数,可以被派生类重写.
-    // 2. 并在运行时动态绑定到对象的实际类型.
-    // 3. 通过基类指针或引用调用该虚函数时,
-    //    程序会根据实际对象的类型调用正确的函数版本,
-    //    而不是仅仅根据指针或引用的类型来决定调用哪个函数.
     virtual void Print(std::string message) {
         std::cout << "Base: " + message << std::endl;
     }
@@ -23,6 +23,44 @@ public:
     }
 };
 
+class Animal
+{
+public:
+    // =0 纯虚函数
+    virtual void Run() = 0;
+    virtual void Waa() = 0;
+};
+
+class Dog: public Animal
+{
+public:
+    void Run() override {
+        std::cout << "dog Run ..." << std::endl;
+    }
+
+    void Waa() override {
+        std::cout << "dog Waa ..." << std::endl;
+    }
+};
+
+class Cat: public Animal
+{
+public:
+    void Run() override {
+        std::cout << "cat Run ..." << std::endl;
+    }
+
+    void Waa() override {
+        std::cout << "cat Waa ..." << std::endl;
+    }
+};
+
+void AnimalAction(Animal *a)
+{
+    a->Run();
+    a->Waa();
+}
+
 int main()
 {
     // 通过基类指针调用
@@ -34,6 +72,12 @@ int main()
     Derive d;
     Base &bb = d;
     bb.Print("hello wagawaga");
+
+    // 接口抽象类
+    Dog dog;
+    Cat cat;
+    AnimalAction(&dog);
+    AnimalAction(&cat);
 
     return 0;
 }
